545908dafdb758310d7669ff07affd0346735287
vikram <vikramamin22@gmail.com>
2021-08-26 22:14:44 +0530

    view product details page is done

---
+++ viewproduct.js
@@ -0,0 +1,232 @@
+
+let productlist = [
+  {
+    name: "MSI GE66 Core i7 11th Gen - (16 GB/1 TB SSD/Windows 10 Home/8 GB Graphics/NVIDIA GeForce RTX 3070/165 ...",
+    price: 98256,
+    img: "https://rukminim1.flixcart.com/image/312/312/kfpq5jk0/computer/h/t/b/msi-na-gaming-laptop-original-imafw3yhmdkzmhzr.jpeg?q=70",
+  },
+  {
+    name: "MSI GF63 Thin Core i5 9th Gen - (8 GB/512 GB SSD/Windows 10 Home/4 GB Graphics/NVIDIA GeForce GTX 1650...",
+    price: 56990,
+    img: "https://rukminim1.flixcart.com/image/312/312/ke353m80/computer/p/q/m/msi-original-imafuurxjxzy4zcn.jpeg?q=70",
+  },
+  {
+    name: "MSI Modern 14 Core i7 11th Gen - (8 GB/512 GB SSD/Windows 10 Home) Modern 14 B11MO-092IN Thin and Ligh...",
+    price: 69590,
+    img: "https://rukminim1.flixcart.com/image/312/312/klzhq4w0/computer/s/k/x/na-thin-and-light-laptop-msi-original-imagyzpcazfbmhjp.jpeg?q=70",
+  },
+  {
+    name: "MSI GP65 Leopard Core i7 10th Gen - (16 GB/1 TB HDD/256 GB SSD/Windows 10 Home/6 GB Graphics/NVIDIA Ge...",
+    price: 25000,
+    img: "https://rukminim1.flixcart.com/image/312/312/kmxsakw0/computer/f/g/t/na-laptop-msi-original-imagfq9cthg4wxwh.jpeg?q=70",
+  },
+  {
+    name: "Acer Predator Helios 300 Core i7 10th Gen - (16 GB/1 TB SSD/Windows 10 Home/6 GB Graphics/NVIDIA GeFor..",
+    price: 40000,
+    img: "https://rukminim1.flixcart.com/image/312/312/kmwcuq80/computer/d/z/k/na-gaming-laptop-acer-original-imagfp8wrqwe6v6w.jpeg?q=70",
+  },
+  {
+    name: "Acer Nitro Core i5 11th Gen - (8 GB/1 TB HDD/256 GB SSD/Windows 10 Home/4 GB Graphics/NVIDIA GeForce R... ",
+    price: 82000,
+    img: "https://rukminim1.flixcart.com/image/312/312/kmns7m80/computer/g/5/p/na-gaming-laptop-acer-original-imagfgq6mjjqwwhq.jpeg?q=70",
+  },
+  {
+    name: "Acer Aspire 7 Ryzen 5 Quad Core 3550H - (8 GB/512 GB SSD/Windows 10 Home/4 GB Graphics/NVIDIA GeForce ...",
+    price: 30000,
+    img: "https://rukminim1.flixcart.com/image/312/312/kcc9q4w0/computer/h/t/g/acer-na-gaming-laptop-original-imafthr8aa4rsjpn.jpeg?q=70",
+  },
+  {
+    name: "HP Pavilion Gaming Ryzen 5 Hexa Core 4600H - (8 GB/1 TB HDD/Windows 10 Home/4 GB Graphics/NVIDIA GeForce GTX 1650",
+    price: 59998,
+    img: "https://rukminim1.flixcart.com/image/312/312/kcm9t3k0/computer/q/x/r/hp-na-gaming-laptop-original-imaftpesbvfxgw9t.jpeg?q=70",
+  },
+  {
+    name: "HP Spectre x360 Core i7 10th Gen - (16 GB/512 GB SSD/Windows 10 Pro) 13-aw0205tu 2 in 1 Laptop",
+    price: 92332,
+    img: "https://rukminim1.flixcart.com/image/312/312/kcauaa80/computer/g/y/6/hp-na-gaming-laptop-original-imaftgf9juhhfgyz.jpeg?q=70",
+  },
+  {
+    name: "HP Spectre x360 Core i7 11th Gen - (16 GB/1 TB SSD/Windows 10 Home) 13-aw2002TU 2 in 1 Laptop HP Spectre x360 Core i7 11th Gen",
+    price: 36000,
+    img: "https://rukminim1.flixcart.com/image/312/312/k8ro3gw0/computer/z/p/q/hp-na-original-imafqpnf5tthbuvh.jpeg?q=70",
+  },
+  {
+    name: "HP Omen Core i5 7th Gen - (8 GB/1 TB HDD/128 GB SSD/Windows 10 Home/4 GB Graphics/NVIDIA GeForce GTX 1050) 15-ce071TX Gaming Laptop ",
+    price: 72100,
+    img: "https://rukminim1.flixcart.com/image/312/312/j6xxgnk0/computer/9/j/k/hp-gaming-laptop-original-imaexaznx4sdxhac.jpeg?q=70",
+  },
+  {
+    name: "ASUS ROG Strix G15 (2021) Ryzen 5 Hexa Core 5600H - (8 GB/1 TB SSD/Windows 10 Home/4 GB Graphics/NVIDIA GeForce RTX 3050/144 Hz) ",
+    price: 95000,
+    img: "https://rukminim1.flixcart.com/image/312/312/ko382a80/computer/b/v/u/na-gaming-laptop-asus-original-imag2mrvrxgsaahh.jpeg?q=70",
+  },
+  {
+    name: "ASUS ROG Strix G15 (2020) Octa Core i7 10th Gen - (16 GB/1 TB SSD/Windows 10 Home/4 GB Graphics/NVIDIA GeForce GTX 1650 Ti/144 Hz)",
+    price: 82000,
+    img: "https://rukminim1.flixcart.com/image/312/312/ks7tuvk0/computer/t/j/v/g513qy-hq008ts-gaming-laptop-asus-original-imag5tthdvxrbrnv.jpeg?q=70",
+  },
+  {
+    name: "ASUS ROG Strix G15 (2021) Advantage Edition Ryzen 9 Octa Core 5900HX - (16 GB/1 TB SSD/Windows 10 Home/12 GB Graphics/AMD Radeon RX 6800M)",
+    price: 28562,
+    img: "https://rukminim1.flixcart.com/image/312/312/koudfgw0/computer/l/s/l/g513qe-hn115t-gaming-laptop-asus-original-imag37djathfqh4h.jpeg?q=70",
+  },
+  {
+    name: "ASUS TUF Gaming F15 (2021) Core i5 11th Gen - (16 GB/512 GB SSD/Windows 10 Home/6 GB Graphics/NVIDIA GeForce RTX 3060/144 Hz)",
+    price: 100000,
+    img: "https://rukminim1.flixcart.com/image/312/312/kpodocw0/computer/9/s/7/na-gaming-laptop-asus-original-imag3v4hsgz62qgw.jpeg?q=70",
+  },
+];
+
+if (localStorage.getItem("productlist") == null) {
+  localStorage.setItem("productlist", JSON.stringify(productlist));
+}
+
+function appendpro(d) {
+
+  let data = d;
+  let mainDiv = document.getElementById("productgrid");
+  mainDiv.innerHTML = null;
+
+  data.forEach(function (el) {
+    let div = document.createElement("div");
+    let p_name = document.createElement("p");
+    p_name.innerHTML = el.name;;
+    p_name.setAttribute("class", "FDname");
+
+    let p_Price = document.createElement("ul");
+    p_Price.innerHTML = "MRP -- ₹ " + el.price;
+    p_Price.style.fontSize = "22px";
+    p_Price.setAttribute("id", "FDprice");
+
+    let img = document.createElement("img");
+    img.src = el.img;
+    img.setAttribute("class", "FDimg");
+    div.append(img, p_name, p_Price);
+    mainDiv.append(div);
+
+    div.addEventListener("click", function () {
+      window.open("showProductdetails.html");
+      showdetail(el);
+    });
+  });
+}
+appendpro(JSON.parse(localStorage.getItem("productlist")));
+
+
+function showdetail(el) {
+  localStorage.setItem("prodetaails", JSON.stringify(el));
+}
+
+
+function searchBybrand(el) {
+  let data = JSON.parse(localStorage.getItem("productlist"));
+  let arr = [];
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].name.split(" ")[0] === el) {
+      arr.push(data[i]);
+    }
+  }
+  appendpro(arr);
+}
+
+let MSI = document.getElementById("MSI");
+MSI.addEventListener("click", function () {
+  searchBybrand("MSI");
+});
+
+let Acer = document.getElementById("Acer");
+Acer.addEventListener("click", function () {
+  searchBybrand("Acer");
+});
+
+let HP = document.getElementById("HP");
+HP.addEventListener("click", function () {
+  searchBybrand("HP");
+});
+
+let ASUS = document.getElementById("ASUS");
+ASUS.addEventListener("click", function () {
+  searchBybrand("ASUS");
+});
+
+let searchbar = document.getElementById("searchbar");
+let submit = document.getElementById("sumitbtn");
+submit.addEventListener("click", function () {
+  let data = JSON.parse(localStorage.getItem("productlist"));
+  let arr = [];
+  let valeofuser = searchbar.value;
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].name.split(" ")[0] === valeofuser) {
+      arr.push(data[i]);
+    }
+  }
+  appendpro(arr);
+});
+
+let first = document.getElementById("25000-40000");
+first.addEventListener("click", function () {
+  pricehunt(first.value);
+});
+
+let second = document.getElementById("40000-60000");
+second.addEventListener("click", function () {
+  pricehunt(second.value);
+});
+let third = document.getElementById("60000-80000");
+third.addEventListener("click", function () {
+  pricehunt(third.value);
+});
+let forth = document.getElementById("80000-100000");
+forth.addEventListener("click", function () {
+  pricehunt(forth.value);
+});
+// let fifth = document.getElementById("463-1728");
+// fifth.addEventListener("click", function () {
+//   pricehunt(fifth.value);
+// });
+
+//sort by price
+function pricehunt(el) {
+  let newel = el.split("-").map(Number);
+  let low = newel[0];
+  let high = newel[1];
+  let arr = [];
+  let data = JSON.parse(localStorage.getItem("productlist"));
+
+  for (var i = 0; i < data.length; i++) {
+    if (Number(data[i].price) <= high || data.price >= low) {
+      arr.push(data[i]);
+       console.log(data[i]);
+    }
+  }
+ 
+  appendpro(arr);
+}
+
+
+
+document.getElementById("highttolow").addEventListener("click", function () {
+  sortHL();
+});
+document.getElementById("lowtohigt").addEventListener("click", function () {
+  sortLH();
+});
+
+
+function sortLH() {
+  let data = JSON.parse(localStorage.getItem("productlist"));
+  data.sort(function (a, b) {
+    return a.price - b.price;
+  });
+  //  console.log(data)
+  appendpro(data);
+}
+
+function sortHL() {
+  let data = JSON.parse(localStorage.getItem("productlist"));
+  data.sort(function (a, b) {
+    return b.price - a.price;
+  });
+  //  console.log(data)
+  appendpro(data);
+}
