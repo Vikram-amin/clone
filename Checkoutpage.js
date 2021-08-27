b0c986b70f0f8331648bb64c18858dcf9f64ee07
vikram <vikramamin22@gmail.com>
2021-08-27 19:57:10 +0530

    payment page is done

--- Checkoutpage.js
+++ Checkoutpage.js
@@ -1,41 +1,42 @@
+function appenderforchkout() {
+  let sum1 = JSON.parse(localStorage.getItem(`cartsum1`));
+  document.getElementById("ammount").innerHTML = "₹  " + sum1;
+    let sum2 = JSON.parse(localStorage.getItem(`cartsum2`));
+    document.getElementById("ammounttotal").innerHTML = "₹  " + sum2;
+}
+appenderforchkout();
 
-
-
-  function addAddress() {
-    const form = document.getElementById("addressForm");
-
-    const name = form.name.value;
-    const mobileNo = form.mobileNo.value;
-    const pincode = form.pincode.value;
-    const address = form.address.value;
-    const location = form.location.value;
-    const city = form.data2.value;
-    const state = form.data1.value;
-
-    let address1 = {
-      name,
-      mobileNo,
-      pincode,
-      address,
-      location,
-      city,
-      state,
-    };
-    for (k in address1) {
-      if (address1[k].length === 0) {
-        alert("Please fill all feilds");
-        return;
-      }
-    }
-
+function validation() {
+  let name = document.getElementById(`fname`);
+  let email = document.getElementById(`email`);
+  let address = document.getElementById(`adr`);
+  let city = document.getElementById(`city`);
+  let state = document.getElementById(`state`);
+  let zip = document.getElementById(`zip`);
+  let cname = document.getElementById(`cname`);
+  let cnum = document.getElementById("cnum");
+  let expmonth = document.getElementById(`expmonth`);
+  let expyear = document.getElementById(`expyear`);
+  let cvv = document.getElementById(`cvv`);
+  if (
+    email.value == "" ||
+    name == "" ||
+    address == "" ||
+    city == "" ||
+    state == "" ||
+    zip == "" ||
+    cname == "" ||
+    cnum == "" ||
+    cvv == "" ||
+    expmonth == "" ||
+    expyear == ""
+  ) {
+    alert(`all filed need to be field`);
+  } else {
+    window.location.href = "paymentDone.html";
   }
-
-
-
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
 }
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
+document.getElementById(`donechek`).onclick = function () {
+  validation();
+};
